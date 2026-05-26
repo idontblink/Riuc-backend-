2fa19ae30b68575ed30f114c16928efe4a3dea49
Uel Akpeh <akpehuel@gmail.com>
2026-05-26 19:04:38 +0000

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,11 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL || 
-    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: {
-    rejectUnauthorized: false // Required for Supabase
-  }
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.on('error', (err) => {
