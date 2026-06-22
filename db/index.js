aa87768c9b9ffc06babca2ad6a07ba38fda8341a
Uel Akpeh <akpehuel@gmail.com>
2026-06-22 02:01:11 +0000

    fix: clean db pool

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
 });
 
 module.exports = pool;
